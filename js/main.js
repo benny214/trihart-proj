815f141568de4d8774f8277a3c0c4ab763ec4215
vtumanov <viktor.tumanov@waveaccess.ru>
2016-06-02 15:34:19 +0300

    Email test

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
-//E-mail Ajax Send
-  $(".nav_form").submit(function() {
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php",
-      data: th.serialize()
-    }).done(function() {
-      alert("Thank you!");
-      setTimeout(function() {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
+// //E-mail Ajax Send
+//   $(".nav_form").submit(function() {
+//     var th = $(this);
+//     $.ajax({
+//       type: "POST",
+//       url: "mail.php",
+//       data: th.serialize()
+//     }).done(function() {
+//       alert("Thank you!");
+//       setTimeout(function() {
+//         // Done Functions
+//         th.trigger("reset");
+//       }, 1000);
+//     });
+//     return false;
+//   });
 
 //fullpage scroll
   $('#fullpage').fullpage({
